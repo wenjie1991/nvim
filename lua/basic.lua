083ce4c23e86e19df713c6d82fa3ef734b5a9175
Wenjie Sun <wenjie1991@users.noreply.github.com>
2024-03-15 10:35:19 +0100

    updata imac part

--- lua/basic.lua
+++ lua/basic.lua
@@ -186,3 +186,9 @@ let g:matchparen_insert_timeout = 2
 vim.cmd[[
 let g:slime_target = "neovim"
 ]]
+
+-- blink cursor
+-- vim.cmd[[
+-- set guicursor=n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175
+-- " set guicursor=a:blinkon100
+-- ]]
