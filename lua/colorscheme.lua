1714c82b88ceb0967460fb2bf1a7b91ceaae9db8
wenjie1991 <sunwjie@gmail.com>
2024-06-01 18:36:42 +0200

    Update Copilot color

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,15 +1,16 @@
 -- local colorscheme = "onedark"
 -- vim.o.background = "dark"
-vim.o.background = "light"
+-- vim.o.background = "light"
 -- local colorscheme = "solarized-high"
 -- local colorscheme = "github_light"
+
 
 -- begin: vscode theme configuration
 local colorscheme = "vscode"
 local c = require('vscode.colors').get_colors()
 require('vscode').setup({
     -- Alternatively set style in setup
-    -- style = 'light'
+    style = 'light',
 
     -- Enable transparent background
     transparent = false,
@@ -21,15 +22,16 @@ require('vscode').setup({
     disable_nvimtree_bg = true,
 
     -- Override colors (see ./lua/vscode/colors.lua)
-    -- color_overrides = {
-        -- vscLineNumber = '#000000',
-    -- },
+    color_overrides = {
+        vscLineNumber = '#000000',
+    },
 
     -- Override highlight groups (see ./lua/vscode/theme.lua)
     group_overrides = {
         -- this supports the same val table as vim.api.nvim_set_hl
         -- use colors from this colorscheme by requiring vscode.colors!
         Cursor = { fg=c.vscDarkBlue, bg=c.vscLightGreen, bold=true },
+        CopilotSuggestion = { fg = "#555555", ctermfg = 8, force = true },
     }
 })
 require('vscode').load()
